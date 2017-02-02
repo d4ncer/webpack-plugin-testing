579c4a3be5f8b6088c45b0b28beb6e44bbd0bb5e
Nicolas Maquet <nicolas@movio.co>
2017-02-02 19:21:54 +1300

    Extract plugin in separate file

---
+++ plugins/dependency-graph-plugin.js
@@ -0,0 +1,45 @@
+const exec = require('child_process').exec;
+
+function DependencyGraphPlugin(options) {
+  this.options = options;
+}
+
+DependencyGraphPlugin.prototype.apply = function(compiler) {
+  const plugin = this;
+  const lines = [];
+  compiler.plugin('after-compile', function(compilation, callback) {
+    lines.push('digraph G {');
+    compilation.modules.forEach((m) => {
+      m.dependencies.filter(d => d.module !== null).forEach((d) => {
+        lines.push(`"${m.rawRequest}" -> "${d.module.rawRequest}";`);
+      });
+    });
+    lines.push("}");
+    callback();
+  });
+  compiler.plugin('emit', function(compilation, callback) {
+    const contents = lines.join('\n');
+    lines.splice(0);
+    compilation.assets[plugin.options.output || 'deps-graph.dot'] = {
+      source: function() {
+        return contents;
+      },
+      size: function() {
+        return contents.length;
+      }
+    };
+    callback();
+  });
+  compiler.plugin('after-emit', function(compilation, callback) {
+    if (plugin.options.postBuildCommand) {
+      exec(plugin.options.postBuildCommand, { cwd: this.options.output.path }, (err) => {
+        if (err) {
+          console.error(err);
+        }
+        callback();
+      });
+    }
+  });
+};
+
+module.exports = DependencyGraphPlugin;
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
-const exec = require('child_process').exec;
-
-function DependencyGraphPlugin(options) {
-  this.options = options;
-}
-
-DependencyGraphPlugin.prototype.apply = function(compiler) {
-  const plugin = this;
-  const lines = [];
-  compiler.plugin('after-compile', function(compilation, callback) {
-    lines.push('digraph G {');
-    compilation.modules.forEach((m) => {
-      m.dependencies.filter(d => d.module !== null).forEach((d) => {
-        lines.push(`"${m.rawRequest}" -> "${d.module.rawRequest}";`);
-      });
-    });
-    lines.push("}");
-    callback();
-  });
-  compiler.plugin('emit', function(compilation, callback) {
-    const contents = lines.join('\n');
-    lines.splice(0);
-    compilation.assets[plugin.options.output || 'deps-graph.dot'] = {
-      source: function() {
-        return contents;
-      },
-      size: function() {
-        return contents.length;
-      }
-    };
-    callback();
-  });
-  compiler.plugin('after-emit', function(compilation, callback) {
-    if (plugin.options.postBuildCommand) {
-      exec(plugin.options.postBuildCommand, { cwd: this.options.output.path }, (err) => {
-        if (err) {
-          console.error(err);
-        }
-        callback();
-      });
-    }
-  });
-};
+const DependencyGraphPlugin = require('./plugins/dependency-graph-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, './src'),
